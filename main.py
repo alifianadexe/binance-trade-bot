4a3193f3ac052c8f240ff54f2fbcb5c60ad29e6a
alifianadexe <alifianadexe@gmail.com>
2021-03-29 22:16:39 +0700

    nice change

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from twisted.internet import reactor
 from config import user_api
 
 import pandas as pd
-import json
-import csv
+import btalib
 
 bnb_price = {'error':False}
 
@@ -18,7 +17,7 @@ def bnb_trade_history(msg):
     else:
         print("Error!")        
         
-client = Client(user_api.API_KEY, user_api.SECRET_KEY)
+# client = Client(user_api.API_KEY, user_api.SECRET_KEY)
 
 # open the websocket
 # bsm  = BinanceSocketManager(client)
@@ -29,18 +28,39 @@ client = Client(user_api.API_KEY, user_api.SECRET_KEY)
 # valid intervals - 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
 
 # get timestamp of earliest date data availabel
-timestamp = client._get_earliest_valid_timestamp('BNBUSDT', '1d')
-print(timestamp)
+# timestamp = client._get_earliest_valid_timestamp('BNBUSDT', '1d')
 
-bars = client.get_historical_klines('BTCUSDT', '1d', timestamp, limit=1000)
+# get klines
+# bars = client.get_historical_klines('BNBUSDT', '1d', timestamp, limit=1000)
 
 # delete unwanted data
-for line in bars:
-    del line[5:]
+# for line in bars:
+#     del line[5:]
 
-bnb_df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close'])
-bnb_df.set_index('date', inplace=True)
-print(bnb_df.head())
+# bnb_df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close'])
+# bnb_df.set_index('date', inplace=True)
+# print(bnb_df.head())
+
+# bnb_df.to_csv("bnb_bars.csv")
+
+
+bnb_df = pd.read_csv('bnb_bars.csv', index_col=0)
+
+bnb_df.index = pd.to_datetime(bnb_df.index, unit='ms')
+
+rsi = btalib.rsi(bnb_df, period=10) 
+macd = btalib.macd(bnb_df, pfast=20, pslow=50, psignal=13)
+
+bnb_df['sma5'] = btalib.sma(bnb_df.close, period=5).df
+bnb_df['sma10'] = btalib.sma(bnb_df.close, period=10).df
+bnb_df['sma20'] = btalib.sma(bnb_df.close, period=20).df
+
+bnb_df = bnb_df.join([rsi.df, macd.df])
+
+print(bnb_df.tail())
+
+
+
 
 
 
